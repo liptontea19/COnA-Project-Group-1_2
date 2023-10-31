aeea5ca268770e00aec5dd8c86d6e7f4b07d2604
prezgamer <ffjandrew@yahoo.com.sg>
2023-10-31 18:26:02 +0800

    Updated example
    Pls work;}

--- DeleteWhenDone/example.s
+++ DeleteWhenDone/example.s
@@ -1,15 +1,19 @@
-    // first_asm.s; Defines a main function to add 2 numbers; 
-    // data sector is for initialized variables and constants
-    .data
-    // text sector is for the actual code;
-    .text
-    .global  main                         // start the assembly code;
+//hello_world.s; printf"Hello World!" in assembly code;
+//data sector is for initialized variables and constants
+.data
+string: .asciz "\nHello World!\n" // for printf calls, by GNU assembler’s ".asciz";
+//text sector is for the actual code;
+.text
+.global main //start the assembly code;
+.extern printf //external function printf;
 
-     main: 
-                  MOV      X1, #8         // put the first value  in register R1;
-                  MOV      X2, #9         // put the second value in register R2;
-                  ADD      X0, X1, X2     // add X0 = X1 + X2;
-                  RET             // end program; return to previous instruction; LR: Link Reg.
-
-
-
+main:
+    SUB sp,sp,#16 // Store the return address, i.e. link reg. (lr), and
+    STR X30, [sp] // Save the return lr to stack.
+    
+    LDR X0, =string // Load string into X0 to perform procedural call.
+    BL printf // branch link to printf, like 'printf("...")' in C.
+    
+    LDR X30, [sp] // Access the OS
+    ADD sp, sp, #16 // get back the lr (link reg.) value, return;
+    RET
