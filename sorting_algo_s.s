91ab69b8558d508d1daef13fd3050f71af81ae02
prezgamer <ffjandrew@yahoo.com.sg>
2023-10-31 17:05:05 +0800

    example

--- sorting_algo_s.s
+++ sorting_algo_s.s
@@ -1,18 +1,13 @@
-section .data
-    hello db 'Hello, World!',0  ; Define a null-terminated string
+.global main
 
-section .text
-global _start
-
-_start:
-    ; Write "Hello, World!" to stdout
-    mov eax, 4                 ; System call number for sys_write (4)
-    mov ebx, 1                 ; File descriptor for stdout (1)
-    mov ecx, hello             ; Pointer to the string to write
-    mov edx, 13                ; Length of the string
-    int 0x80                   ; Invoke the kernel
-
-    ; Exit the program
-    mov eax, 1                 ; System call number for sys_exit (1)
-    mov ebx, 0                 ; Exit with status code 0
-    int 0x80                   ; Invoke the kernel
+main:
+MOV R0,#3
+MOV R1,#0
+LOOP:
+CMP R0,R1
+BEQ END
+ADDGT R1,#1
+SUBLT R1.#1
+B LOOP
+END:
+BX LR
