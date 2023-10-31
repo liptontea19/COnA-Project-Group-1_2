86d92f295decedf0da53865f6e5b605ec17a5677
prezgamer <ffjandrew@yahoo.com.sg>
2023-10-31 17:08:35 +0800

    example

--- sorting_algo_s.s
+++ sorting_algo_s.s
@@ -1,13 +1,13 @@
 .global main
 
 main:
-MOV R0,#3
-MOV R1,#0
+MOV R0,#3;
+MOV R1,#0;
 LOOP:
-CMP R0,R1
-BEQ END
-ADDGT R1,#1
-SUBLT R1.#1
-B LOOP
+CMP R0,R1;
+BEQ END;
+ADDGT R1,#1;
+SUBLT R1.#1;
+B LOOP;
 END:
-BX LR
+BX LR;
