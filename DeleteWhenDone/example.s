e5c061916c0df30f871f76d672e2a37beedad6c7
prezgamer <ffjandrew@yahoo.com.sg>
2023-11-01 19:25:19 +0800

    Hope this code works

--- DeleteWhenDone/example.s
+++ DeleteWhenDone/example.s
@@ -1,17 +1,110 @@
 .data
-string: .asciz "\nHello World!\n"
+array:      .skip MAX_SIZE * 4      // Define an array to be sorted (each element is 4 bytes)
+MAX_SIZE:   .word 10000            // Define the maximum size of the array
 
 .text
-.global main
-.extern printf
+.global _start
 
-main:
-    SUB sp,sp,#16
-    STR X30,[sp]
+_start:
+    // Initialize stack pointer
+    MOV sp, #0x8000                // Set stack pointer (adjust the value as needed)
 
-    LDR X0, =string
-    BL printf
-    
-    LDR X30,[sp]
-    ADD sp,sp,#16
-    RET
+    // Initialize the array with random values
+    LDR r1, =array                 // Load the address of the array
+    LDR r2, =MAX_SIZE              // Load the number of elements
+    BL initialize_array
+
+    @ Call the quick_sort function
+    LDR r1, =array                 // Load the address of the array
+    LDR r2, =0                     // Load the first index
+    LDR r3, =MAX_SIZE - 1           // Load the last index
+    BL quick_sort
+
+    @ Terminate the program
+    MOV r7, #1                      // syscall number for exit
+    MOV r0, #0                      // Exit status
+    SWI 0                           // Make a syscall to exit
+
+initialize_array:
+    // Arguments:
+    // r1 = Address of the array
+    // r2 = Number of elements
+
+    loop_initialize:
+        CMP r2, #0
+        BEQ exit_initialize
+
+        // Generate a random number AND store it in the array
+        BL generate_random
+        STR r0, [r1], #4    // Store the random number AND increment the pointer
+        SUB r2, r2, #1      // Decrement the number of elements
+        B loop_initialize
+
+    exit_initialize:
+        BX LR
+
+generate_random:
+    // Generate a random number AND store it in r0
+    // You may need to implement your own random number generator
+    // This example uses a simple XOR-shift random number generator
+    // You can replace this with a more suitable random number generation method
+
+    LDR r4, =0x5F748241       // Set initial seed (can be any value)
+    LSL r4, r4, #13           // Left shift the seed
+    EOR r4, r4, r4, LSR #17   // XOR the seed with a right-shifted version of itself
+    EOR r4, r4, r4, LSL #5    // XOR the seed with a left-shifted version of itself
+
+    LDR r5, =0x7FFFFFFF        // Set a bitmask to keep the result positive
+
+    AND r0, r4, r5            // Apply the bitmask to get a positive random number
+    BX LR
+
+quick_sort:
+    // Arguments:
+    // r1 = Address of the array
+    // r2 = First index
+    // r3 = Last index
+
+    CMP r2, r3
+    BGE exit_quick_sort
+
+    // Call the partition function
+    BL partition
+    MOV r4, r0                 // r4 = pivot index
+
+    // Recursively call quick_sort for the left AND right partitions
+    ADD r3, r4, #-1            // r3 = pivot index - 1
+    BL quick_sort
+    ADD r2, r4, #1             // r2 = pivot index + 1
+    BL quick_sort
+
+    exit_quick_sort:
+        BX LR
+
+partition:
+    // Arguments:
+    // r1 = Address of the array
+    // r2 = First index
+    // r3 = Last index
+
+    // You can implement the partitioning logic here
+    // This example uses the first element as the pivot
+    // You may want to replace this with a more robust partitioning algorithm
+
+    LDR r4, [r1, r2, LSL #2]   // Load the pivot value (assuming 4-byte integers)
+    MOV r5, r2                 // r5 = current index
+
+    loop_partition:
+        ADD r5, r5, #1          // Increment the current index
+        LDR r6, [r1, r5, LSL #2]  // Load the current element
+
+        CMP r6, r4
+        BGE loop_partition
+
+        // Swap r6 AND r5 elements
+        STR r6, [r1, r5, LSL #2]
+        STR r4, [r1, r2, LSL #2]
+
+    exit_partition:
+        MOV r0, r5
+        BX LR
