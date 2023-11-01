bfe72e07bfb57c15edf9f6767e4ee62d2939ff6a
prezgamer <ffjandrew@yahoo.com.sg>
2023-11-01 16:31:14 +0800

    Created Hello World to test

--- DeleteWhenDone/example.s
+++ DeleteWhenDone/example.s
@@ -1,119 +1,22 @@
 .section .data
-MAX_SIZE = 10000
-x: .skip MAX_SIZE, 4    
-
-.section .bss
-start_time: .xword 0
-end_time: .xword 0
+hello_message:
+    .ascii "Hello, World!\n"
+    message_len = . - hello_message
 
 .section .text
 .global _start
 
 _start:
-    // Call the 'main' function
-    bl main
+    // Prepare the syscall arguments
+    mov x0, 1          // File descriptor for stdout (1)
+    ldr x1, =hello_message  // Pointer to the message
+    mov x2, message_len // Message length
 
-    // Your program's cleanup code goes here
+    // Call the write syscall (syscall number 64 on AArch64)
+    mov x8, 64         // syscall number for write
+    svc 0              // Make the syscall
 
-    // Exit the program
-    mov x8, #93   // __NR_exit (syscall for program exit)
-    mov x0, #0    // Exit status
-    svc #0        // Invoke syscall
-
-main:
-    // Initialize stack pointer and call the quick_sort function
-    mov x0, sp
-    ldr x1, =0
-    ldr x2, =MAX_SIZE
-    bl quick_sort
-
-    // Measure execution time
-    ldr x8, =start_time
-    ldr x9, =end_time
-    bl measure_time
-
-    // Add code to print execution time here
-
-    // Exit the program
-    mov x8, #93   // __NR_exit
-    mov x0, #0
-    svc #0
-
-measure_time:
-    // Measure execution time using platform-specific instructions or syscalls
-    // Store the start time in x8 and the end time in x9
-    // Replace this part with platform-specific timing code
-    // You may need to make a syscall to the operating system or use platform-specific instructions
-
-    // Return with x8 and x9 containing the start and end times
-    ret
-
-quick_sort:
-    stp x29, x30, [sp, #-16]!  // Push the link register and frame pointer onto the stack
-    mov x29, sp                // Set up the new frame pointer
-
-    ldr x3, [x0]               // Load the base address of the array
-    cmp x2, x1                 // Compare first and last indices
-    bge .L1                    // If first >= last, return
-
-    // Call the partition function
-    bl partition
-
-    // Recursively call quick_sort on the left and right partitions
-    ldr x4, [sp, #16]          // Load the first argument (base address) from the stack
-    mov x2, x5                 // Update 'last' with the pivot index - 1
-    bl quick_sort
-
-    ldr x4, [sp, #16]          // Load the first argument (base address) from the stack
-    mov x1, x5                 // Update 'first' with the pivot index + 1
-    ldr x6, [sp, #16]          // Load the third argument (last) from the stack
-    bl quick_sort
-
-    // Pop the frame and link register, and return
-.L1:
-    ldp x29, x30, [sp], 16
-    ret
-
-partition:
-    stp x29, x30, [sp, #-16]!  // Push the link register and frame pointer onto the stack
-    mov x29, sp                // Set up the new frame pointer
-
-    ldr x3, [x0]               // Load the base address of the array
-    ldr x4, [x1]               // Load the pivot index
-    ldr w5, [x3, x4, lsl #2]   // Load the pivot value (assuming 32-bit integers)
-
-    // Partition the array into two sub-arrays
-    mov x6, x1                 // i = first
-    sub x7, x2, #1             // j = last - 1
-
-.L2:
-    ldr w8, [x3, x6, lsl #2]   // Load x[i] (assuming 32-bit integers)
-    ldr w9, [x3, x7, lsl #2]   // Load x[j] (assuming 32-bit integers)
-
-    // Compare x[i] and pivot value
-    cmp w8, w5
-    bge .L3
-
-    // Swap x[i] and x[j]
-    str w9, [x3, x6, lsl #2]   // Store x[j] in x[i]
-    str w8, [x3, x7, lsl #2]   // Store x[i] in x[j]
-
-.L3:
-    // Update i and j
-    add x6, x6, #1
-    sub x7, x7, #1
-
-    // Check if i <= j
-    cmp x6, x7
-    ble .L2
-
-    // Swap x[i] and pivot value
-    str w5, [x3, x6, lsl #2]   // Store pivot value in x[i]
-    str w8, [x3, x4, lsl #2]   // Store x[i] in pivot position
-
-    // Return the pivot index
-    mov x0, x6
-
-    // Pop the frame and link register, and return
-    ldp x29, x30, [sp], 16
-    ret
+    // Exit the program (syscall number 93 on AArch64)
+    mov x8, 93         // syscall number for exit
+    mov x0, 0          // Exit status
+    svc 0              // Make the syscall
