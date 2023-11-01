92c4d8d374d702bc8489890374178384af172695
prezgamer <ffjandrew@yahoo.com.sg>
2023-11-01 19:03:10 +0800

    Followed Lab 8 Qn 3 Solution
    Hello World

--- DeleteWhenDone/example.s
+++ DeleteWhenDone/example.s
@@ -1,22 +1,13 @@
-.section .data
-hello_message:
-    .ascii "Hello, World!\n"
-    message_len = . - hello_message
+.data
+.text
+.global main
+.extern printf
 
-.section .text
-.global _start
-
-_start:
-    // Prepare the syscall arguments
-    mov x0, 1          // File descriptor for stdout (1)
-    ldr x1, =hello_message  // Pointer to the message
-    mov x2, message_len // Message length
-
-    // Call the write syscall (syscall number 64 on AArch64)
-    mov x8, 64         // syscall number for write
-    svc 0              // Make the syscall
-
-    // Exit the program (syscall number 93 on AArch64)
-    mov x8, 93         // syscall number for exit
-    mov x0, 0          // Exit status
-    svc 0              // Make the syscall
+main:
+    SUB sp,sp,#16
+    STR X30,[sp]
+    LDR X0, =string
+    BL printf
+    LDR X30,[sp]
+    ADD sp,sp,#16
+    RET
