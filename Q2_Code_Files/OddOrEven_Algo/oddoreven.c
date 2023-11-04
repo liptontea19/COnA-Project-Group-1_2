31675196f1ab846a4d5104dc22b740c59941846c
prezgamer <ffjandrew@yahoo.com.sg>
2023-11-04 18:58:56 +0800

    Added timer to oddoreven code

--- Q2_Code_Files/OddOrEven_Algo/oddoreven.c
+++ Q2_Code_Files/OddOrEven_Algo/oddoreven.c
@@ -6,12 +6,18 @@ int main() {
 
     printf("Enter an integer: "); //enter a number
     scanf("%d", &num);
+    clock_t t = clock(); //start the timer
 
     // check if it is divisable by 2, if so, it is even, else it is odd
-    if(num % 2 == 0)
-        printf("%d is even.", num);
-    else
-        printf("%d is odd.", num);
-    
+    if(num % 2 == 0) {
+        t = clock() - t;
+        double time_taken = ((double)t) / CLOCKS_PER_SEC;
+        printf("%d is even & it took %f miliseconds to compute", num, time_taken);
+    }
+    else {
+        t = clock() - t;
+        double time_taken = ((double)t) / CLOCKS_PER_SEC;
+        printf("%d is odd & it took %f miliseconds to compute", num, time_taken);
+    }
     return 0;
 }
